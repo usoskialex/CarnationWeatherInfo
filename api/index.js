65e9b7098d0717e3b887f947a55d9a64f007e6cd
Aliaksandr Usoski <usalex93@gmail.com>
2024-05-16 14:00:48 -0700

    debug deployment issues

--- api/index.js
+++ api/index.js
@@ -7,36 +7,42 @@ const port = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', './views')
+console.log(path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 
 
 app.get('/', async (req, res) => {
-    const result = await script.weatherResult;
-    const executionDays = result.ExecutionDays;
-
-    let html = '<div class="wrapper">';
-
-    // Iterate over each execution day array
-    executionDays.forEach(dayArray => {
-        dayArray.forEach(day => {
-            html +=
-            `
-             <div class="dayInfo">
-                <p  id="weather-date"><span class="date-time-format">Date: ${day.day}</span></p>
-                <p  id="weather-time"><span class="date-time-format">Time: ${day.datetime}</span></p>
-                <p  id="weather-temp">Temperature: ${day.temp}&#8457;</p>
-                <p  id="weather-humidity">Humidity: ${day.humidity}%</p>
-                <p  id="weather-visibility">Visibility: ${day.visibility}m</p>
-                <p  id="weather-conditions">Conditions: ${day.conditions}</p>
-             </div>`;
+    try {
+        const result = await script.weatherResult;
+        const executionDays = result.ExecutionDays;
+    
+        let html = '<div class="wrapper">';
+    
+        // Iterate over each execution day array
+        executionDays.forEach(dayArray => {
+            dayArray.forEach(day => {
+                html +=
+                `
+                 <div class="dayInfo">
+                    <p  id="weather-date"><span class="date-time-format">Date: ${day.day}</span></p>
+                    <p  id="weather-time"><span class="date-time-format">Time: ${day.datetime}</span></p>
+                    <p  id="weather-temp">Temperature: ${day.temp}&#8457;</p>
+                    <p  id="weather-humidity">Humidity: ${day.humidity}%</p>
+                    <p  id="weather-visibility">Visibility: ${day.visibility}m</p>
+                    <p  id="weather-conditions">Conditions: ${day.conditions}</p>
+                 </div>`;
+            });
         });
-    });
-    html += '</div>';
-    html += `<p id="weather-average">Average Temperature: ${result['Average Temperature']}&#8457;</p>`;
-    html += `<p id="weather-average">Average Condition: ${result['Average Condition']}</p>`
-    // Send the HTML to the browser
-    res.render('index', { data: html });
+        html += '</div>';
+        html += `<p id="weather-average">Average Temperature: ${result['Average Temperature']}&#8457;</p>`;
+        html += `<p id="weather-average">Average Condition: ${result['Average Condition']}</p>`
+        // Send the HTML to the browser
+        res.render('index', { data: html });
+    } catch (error) {
+        console.error('Error rendering index:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
   
 app.listen(port, () => {
